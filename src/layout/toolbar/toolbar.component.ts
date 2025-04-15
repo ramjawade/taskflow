6b86fdf50be4d03b612f8f49f28a54248064a1f2
ram <ram@ram.lan>
2025-04-16 03:10:12 +0530

    ux fix

--- src/layout/toolbar/toolbar.component.ts
+++ src/layout/toolbar/toolbar.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { IUser } from '../../core/interfaces/user.interface';
 import { UserService } from '../../core/services/user.service';
 import { CommonModule } from '@angular/common';
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+export enum THEME {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
 @Component({
   selector: 'app-toolbar',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
@@ -13,6 +17,10 @@ export class ToolbarComponent implements OnInit {
   user: IUser | null = null;
   userInitials: string = '';
   userAvatarColor: string = '#007bff'; // Default color
+  @Output() toggle = new EventEmitter();
+
+  isOpened: boolean = false;
+  form = new FormGroup({ theme: new FormControl(false) });
 
   constructor(private userService: UserService) {}
 
@@ -22,6 +30,13 @@ export class ToolbarComponent implements OnInit {
       this.userInitials = this.getInitials(this.user.name);
       this.userAvatarColor = this.generateAvatarColor(this.user.name);
     }
+    this.form.get('theme')?.valueChanges.subscribe((isDark) => {
+      if (isDark) {
+        document.body.classList.replace(THEME.LIGHT, THEME.DARK);
+      } else {
+        document.body.classList.replace(THEME.DARK, THEME.LIGHT);
+      }
+    });
   }
 
   private getInitials(name: string): string {
@@ -39,5 +54,16 @@ export class ToolbarComponent implements OnInit {
       .reduce((acc, char) => acc + char.charCodeAt(0), 0);
     const colors = ['#007bff', '#28a745', '#dc3545', '#ffc107', '#17a2b8'];
     return colors[hash % colors.length];
+  }
+
+  toggleButton() {
+    this.isOpened = !this.isOpened;
+    this.toggle.emit(this.isOpened);
+  }
+
+  isDarkTheme(evt: Event) {
+    const flag = (<HTMLInputElement>evt.target).checked;
+    const theme = flag ? THEME.DARK : THEME.LIGHT;
+    document.body.setAttribute('data-bs-theme', theme);
   }
 }
